0c3dbedec510b873fc896752650bd5e3c1fb4633
Omri <117010115+iam0mri@users.noreply.github.com>
2025-11-13 10:42:27 +0100

    squelette profil-posts (#15)
    Co-authored-by: titouangragnic <97364019+titouangragnic@users.noreply.github.com>

---
+++ src/app/(tabs)/profile/settings.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function SettingsScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="screenTitle">Paramètres</ThemedText>
+
+      <Link href="/profile">
+        <ThemedText type="link">Retour au profil</ThemedText>
+      </Link>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 16,
+    padding: 16,
+  },
+});
