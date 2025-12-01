a0032753b8acec80d5071096d398bd4799f31d54
InsMtb <78259423+InsMtb@users.noreply.github.com>
2025-12-01 17:09:47 +0100

    feat: add two steps to the register page with username, bio, etc

--- src/app/auth/register.tsx
+++ src/app/auth/register.tsx
@@ -7,17 +7,23 @@ import { Button } from "@/stories/Button";
 import { InputBar } from "@/stories/InputBar";
 import { router } from "expo-router";
 import React, { useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import {StyleSheet, TouchableOpacity, View} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {Toggle} from "@/stories/Toggle";
 
 export default function Register() {
     const { register } = useUser()!;
-
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [biography, setBiography] = useState("");
+    const [location, setLocation] = useState("");
+    const [isPrivate, setIsPrivate] = useState(false);
+    const [step, setStep] = useState(1);
 
     const handleRegister = async () => {
       const e = await register(email, password);
+      //FIXME: add username, biography, location, isPrivate to the user object
       if (e) {
         alert("Erreur lors de l'inscription: " + e)
       }
@@ -33,30 +39,33 @@ export default function Register() {
                 <ThemedText style={styles.title}>
                     S'inscrire
                 </ThemedText>
-                <InputBar
-                    backgroundColor="background"
-                    hideRightIcon
-                    onChangeText={setEmail}
-                    placeholder="Email"
-                    rightIcon="search"
-                />
-
-                <InputBar
-                    backgroundColor="background"
-                    hideRightIcon
-                    onChangeText={setPassword}
-                    placeholder="Mot de passe"
-                    rightIcon="search"
-                    isPassword={true}
-                />
-
-                <Button
-                    label="S'inscrire"
-                    onPress={handleRegister}
-                    style={styles.loginButton}
-                    size={"large"}
-                />
-
+                {step === 1 &&
+                    <Step1
+                        email={email}
+                        password={password}
+                        setEmail={setEmail}
+                        setPassword={setPassword}
+                        setStep={setStep}/>
+                }
+                {step === 2 &&
+                    <Step2
+                        username={username}
+                        biography={biography}
+                        setUsername={setUsername}
+                        setBiography={setBiography}
+                        setStep={setStep}
+                    />
+                }
+                {step === 3 &&
+                    <Step3
+                        location={location}
+                        setLocation={setLocation}
+                        setIsPrivate={setIsPrivate}
+                        isPrivate={isPrivate}
+                        handleRegister={handleRegister}
+                        setStep={setStep}
+                    />
+                }
                 <TouchableOpacity onPress={() => router.replace("/auth/login")}>
                     <ThemedText style={styles.bottomLinkText}>
                         Vouos avez déjà un compte ? <ThemedText>Connectez-vous</ThemedText>
@@ -65,6 +74,130 @@ export default function Register() {
             </ThemedView>
         </SafeAreaView>
     );
+}
+
+interface Step1Props {
+    email: string,
+    password: string,
+    setEmail: (text: string) => void,
+    setPassword: (text: string) => void;
+    setStep: (step: number) => void;
+}
+
+function Step1({setEmail, setPassword, setStep, email, password}: Step1Props) {
+    return (
+        <>
+            <InputBar
+                backgroundColor="background"
+                hideRightIcon
+                onChangeText={setEmail}
+                placeholder="Email"
+                value={email}
+            />
+
+            <InputBar
+                backgroundColor="background"
+                hideRightIcon
+                onChangeText={setPassword}
+                placeholder="Mot de passe"
+                isPassword={true}
+                value={password}
+            />
+
+            <Button
+                label="Continuer"
+                onPress={() => setStep(2)}
+                style={styles.loginButton}
+                size={"large"}
+            />
+        </>
+    )
+}
+
+interface Step2Props {
+    username: string,
+    biography: string,
+    setUsername: (text: string) => void,
+    setBiography: (text: string) => void;
+    setStep: (step: number) => void;
+}
+function Step2({setUsername, setBiography,  setStep, username, biography}: Step2Props) {
+    return (
+        <>
+            <InputBar
+                backgroundColor="background"
+                hideRightIcon
+                onChangeText={setUsername}
+                placeholder="Nom d'utilisateur"
+                value={username}
+            />
+
+            <InputBar
+                backgroundColor="background"
+                hideRightIcon
+                onChangeText={setBiography}
+                placeholder="Biographie"
+                bigInput
+                value={biography}
+            />
+            <View style={styles.formButtons}>
+                <Button
+                    label="Retour"
+                    onPress={() => setStep(1)}
+                    style={styles.loginButton}
+                    size={"large"}
+                />
+                <Button
+                    label="Continuer"
+                    onPress={() => setStep(3)}
+                    style={styles.loginButton}
+                    size={"large"}
+                />
+            </View>
+        </>
+    )
+}
+
+interface Step3Props {
+    setIsPrivate: (status: boolean) => void,
+    setLocation: (text: string) => void,
+    isPrivate: boolean;
+    location: string;
+    handleRegister: () => void;
+    setStep: (step: number) => void;
+}
+
+function Step3({setLocation, setIsPrivate, isPrivate, handleRegister, setStep, location} : Step3Props) {
+    return (
+        <>
+            <InputBar
+                backgroundColor="background"
+                hideRightIcon
+                onChangeText={setLocation}
+                value={location}
+                placeholder="Ville"
+            />
+            <View style={styles.isPrivateToggle}>
+                <ThemedText>Privé</ThemedText>
+                <Toggle isEnabled={isPrivate} onValueChange={() => setIsPrivate(!isPrivate)}/>
+                <ThemedText>Public</ThemedText>
+            </View>
+            <View style={styles.formButtons}>
+                <Button
+                    label="Retour"
+                    onPress={() => setStep(2)}
+                    style={styles.loginButton}
+                    size={"large"}
+                />
+                <Button
+                    label="Valider l'inscription"
+                    onPress={handleRegister}
+                    style={styles.loginButton}
+                    size={"large"}
+                />
+            </View>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -92,5 +225,15 @@ const styles = StyleSheet.create({
         fontSize: Typography.sizes.title,
         fontWeight: Typography.weights.semiBold,
         margin: Spacing.margin,
+    },
+    isPrivateToggle: {
+        alignSelf: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: Spacing.margin
+    },
+    formButtons:{
+        flexDirection: "row",
+        alignSelf: "center",
     }
 })
