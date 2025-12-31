d05b70b7e1260f0e7b643496c3936303240e2000
InsMtb <78259423+InsMtb@users.noreply.github.com>
2025-12-31 14:04:49 +0100

    fix: use storage according to the platform

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {Platform} from "react-native";
+import * as SecureStore from 'expo-secure-store';
 
 export async function getInStoreUsageOfRealData() : Promise<string> {
+    if(Platform.OS === "ios" || Platform.OS === "android") {
+        return await SecureStore.getItemAsync("usingRealData") ?? "false";
+    }
     return await AsyncStorage.getItem("usingRealData") ?? "false";
 }
